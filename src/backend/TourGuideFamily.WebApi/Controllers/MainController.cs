0e6acc70f584a66b85e8f71be159470ffb3d4ff0
AladinAlex <aladinsky_2001@mail.ru>
2025-03-29 17:29:10 +0300

    Контроллер получения информации для основного сайта

---
+++ src/backend/TourGuideFamily.WebApi/Controllers/MainController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TourGuideFamily.Bll.Services;
+using TourGuideFamily.Bll.Services.Interfaces;
+using TourGuideFamily.WebApi.Models;
+
+namespace TourGuideFamily.WebApi.Controllers;
+
+[ApiController]
+[Route("api/{controller}")]
+public class MainController
+{
+    readonly IGetTourService _getTourService;
+
+    public MainController(IGetTourService getTourService)
+    {
+        _getTourService = getTourService;
+    }
+
+    [HttpGet("Main")]
+    public async Task<IActionResult> Main(CancellationToken token)
+    {
+        try
+        {
+            return new JsonResult(await _getTourService.Main(token));
+        }
+        catch (Exception ex)
+        {
+            return new JsonResult(new ErrorResponse
+            {
+                Error = ex.Message
+            });
+        }
+    }
+
+    [HttpGet("Tour/{tourId}")]
+    public async Task<IActionResult> Tour(long tourId, CancellationToken token)
+    {
+        try
+        {
+            return new JsonResult(await _getTourService.Tour(tourId, token));
+        }
+        catch (Exception ex)
+        {
+            return new JsonResult(new ErrorResponse
+            {
+                Error = ex.Message
+            });
+        }
+    }
+}
