27f497feebe9d89f2439a32b6ceac315c913d071
AladinAlex <aladinsky_2001@mail.ru>
2025-07-24 18:36:13 +0300

    -комент

--- src/frontend/web.client/components/uspCard.vue
+++ src/frontend/web.client/components/uspCard.vue
@@ -12,25 +12,6 @@ const props = defineProps<uspType>();
     <h3>{{ name }}</h3>
     <p>{{ description }}</p>
   </div>
-
-  <!-- <div class="usp-card" data-aos="fade-up">
-        <div class="usp-icon">
-          <img src="/assets/icons/jeep.svg" alt="Джипы" width="48">
-        </div>
-        <h3>Собственный парк внедорожников</h3>
-        <p>
-          Только подготовленные Nissan Safari с кондиционером и треккинговым
-          оборудованием
-        </p>
-      </div>
-
-      <div class="usp-card" data-aos="fade-up" data-aos-delay="100">
-        <div class="usp-icon">
-          <img src="/assets/icons/shield.svg" alt="Безопасность" width="48">
-        </div>
-        <h3>Гиды с 10+ летним опытом</h3>
-        <p>Местные эксперты, знающие каждый каньон и медвежью тропу</p>
-      </div> -->
 </template>
 
 <style scoped lang="scss">
